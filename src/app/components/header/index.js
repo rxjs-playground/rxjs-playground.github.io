c709a1d276160efed6f7dc8c7ade90dcbd18d21e
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-04-15 02:12:27 +0530

    Add github star button

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -5,7 +5,8 @@ export default class Header extends Component{
     return [
       <header id="header">
         <h1> Rx.js Playground </h1>
-        <p> Built with <i className="fa fa-heart"></i> by <a target="_blank" href="http://github.com/bhargav175">bhargav175</a> </p>
+        <p> Built with <i className="fa fa-heart"></i> by <a target="_blank" href="http://github.com/bhargav175">bhargav175</a></p>
+        <p><a className="github-button" href="https://github.com/rxjs-playground/rxjs-playground.github.io" data-icon="octicon-star" data-style="mega" data-count-href="/rxjs-playground/rxjs-playground.github.io/stargazers" data-count-api="/repos/rxjs-playground/rxjs-playground.github.io#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star rxjs-playground/rxjs-playground.github.io on GitHub">Star</a></p>
       </header>
     ]
   }
