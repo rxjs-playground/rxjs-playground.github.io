e8a69cd66ac82e04a8e56ee8d81fcb74bbefc087
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-04-15 17:49:43 +0530

    Add beginner example

--- src/app/content/beginner.js
+++ src/app/content/beginner.js
@@ -63,5 +63,71 @@ oddStream.subscribe(x => console.log(x," is odd"));
 //gets values after the one above
 evenStream.subscribe(x => console.log(x, " is even")); `
     }
+  }, {
+    title : "Implementing a carousel",
+    editor : {
+      html : `<style>
+  #slider, .slides, .slide, .slide img{
+    max-width : 100%;
+  }
+  .slide img{
+    height : 100%;
+    width : 100%;
+  }
+</style>
+
+<div id="slider">
+  <div class="slides">
+    <div class="slide">
+     <img
+      src="http://images.all-free-download.com/images/graphiclarge/beautiful_nature_landscape_03_hd_picture_166205.jpg"/>
+    </div>
+    <div class="slide">
+      <img
+      src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTVTAUJDFwa6KVwUSx5aHJ4TtNhxRLtwwK_OYlCI2ZmUyJCeMzF"/>
+    </div>
+    <div class="slide">
+      <img
+      src="https://www.makemytrip.com/travel-guide/media/dg_image/goa/Nature-Walk.jpg"/>
+    </div>
+  </div>
+  <div>
+    <button id="next"> Next </button> <button id="prev"> Previous </button>
+  </div>
+</div>
+
+`,
+      js : `const slider = document.getElementById("slider");
+const slides = slider.querySelectorAll(".slide");
+
+function showSlide(show){
+  if(show < 0 ){
+    show = slides.length - 1;
+  }
+  if(show >= slides.length){
+    show = 0;
+  }
+  currentSlide = show;
+  slides.forEach((node, index) => {
+    if(index !== show ) {
+      node.setAttribute('style','display : none');
+    }else{
+      node.setAttribute('style','display : block');
+    }
+  });
+}
+let currentSlide = 0 ;
+showSlide(currentSlide);
+
+const nextButton = document.getElementById("next"),
+prevButton = document.getElementById("prev");
+const keyDown = Rx.Observable.fromEvent(document,"keydown");
+const nextButtonClick = Rx.Observable.fromEvent(nextButton,"click")
+const prevButtonClick = Rx.Observable.fromEvent(prevButton,"click")
+const slideClick = Rx.Observable.fromEvent(slides, "click");
+
+Rx.Observable.merge(nextButtonClick, slideClick).subscribe(()=>showSlide(currentSlide + 1));
+Rx.Observable.merge(prevButtonClick, keyDown).subscribe(()=>showSlide(currentSlide - 1));`
+    }
   }
 ]
